3e0b784382ebbcba21a3e9cdc37ce906e2e98f04
Raphael Gisler <raphael.gisler@agilita.ch>
2024-03-11 16:12:29 +0100

    Add quicksort

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,47 @@
 
 fn main() {
-    let data: Vec<i32> = [1,-4,20,-1,500,4141,-3232].to_vec();
-    let sorted = bubble_sort(data.clone());
-    let result = bs(sorted, 20).unwrap();
-    println!("{:?}", result);
+    let mut data: Vec<i32> = [1,-4,20,-1,500,4141,-3232].to_vec();
+    println!("{:?}", data);
+    quicksort(&mut data);
+    println!("{:?}", data);
 }
+
+fn quicksort(arr: &mut Vec<i32>) {
+    qs(arr, 0, (arr.len() - 1) as i32)
+}
+fn qs(arr: &mut Vec<i32>, low: i32, high: i32) {
+    if low >= high {
+        return;
+    }
+
+    let pivot_idx: i32 = pivot(arr, low as usize, high as usize);
+
+    qs(arr, pivot_idx + 1, high);
+    qs(arr, low, pivot_idx - 1);
+}
+fn pivot(arr: &mut Vec<i32>, low: usize, high: usize) -> i32 {
+    let pivot = arr[high];
+
+    let mut idx: i32 = low as i32 - 1;
+
+    for i in low..high {
+        if pivot > arr[i] {
+            idx += 1;
+
+            let temp = arr[idx as usize];
+            arr[idx as usize] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    idx += 1;
+
+    arr[high] = arr[idx as usize];
+    arr[idx as usize] = pivot;
+
+    idx
+}
+
 
 fn bubble_sort(mut arr: Vec<i32>) -> Vec<i32> {
     for i in 0..arr.len() {
